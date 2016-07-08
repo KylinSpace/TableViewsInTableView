6a77c65edca399c5b7feb6cfcd58ec67b27532db
kylin <kylin_up@163.com>
2016-07-08 20:33:31 +0800

    初始化一个项目

---
+++ TableViewsInTableView/TableViewsInTableViewTests/TableViewsInTableViewTests.m
@@ -0,0 +1,39 @@
+//
+//  TableViewsInTableViewTests.m
+//  TableViewsInTableViewTests
+//
+//  Created by Kylin on 16/7/8.
+//  Copyright © 2016年 Kylin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TableViewsInTableViewTests : XCTestCase
+
+@end
+
+@implementation TableViewsInTableViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
